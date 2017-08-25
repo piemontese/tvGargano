53cffc3a0d343d7323573694d27631abfb3666eb
= <pie.piemontese@gmail.com>
2017-08-25 12:35:29 +0200

    new branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { VideoComponent } from './components/video/video.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
+
 const routes: Routes = [
   {
-    path: '',
+    path: '', component: HomeComponent,
+    children: []
+  },
+  {
+    path: 'home', component: HomeComponent,
+    children: []
+  },
+  {
+    path: 'news', component: NewsComponent,
+    children: []
+  },
+  {
+    path: 'video', component: VideoComponent,
+    children: []
+  },
+  {
+    path: 'advertising', component: AdvertisingComponent,
     children: []
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewsComponent } from './components/news/news.component';
+import { VideoComponent } from './components/video/video.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewsComponent,
+    VideoComponent,
+    AdvertisingComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
